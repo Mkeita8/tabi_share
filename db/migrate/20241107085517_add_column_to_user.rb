74eaba061decc12b770f7d29a888b5dd085411b7
宮本敬太 <miyamotokeita@miyamotokeitanoMacBook-Air.local>
2024-11-08 16:10:58 +0900

    devise導入

--- db/migrate/20241107085517_add_column_to_user.rb
+++ db/migrate/20241107085517_add_column_to_user.rb
@@ -1,4 +1,7 @@
 class AddColumnToUser < ActiveRecord::Migration[7.2]
   def change
+    add_column :users, :full_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :avatar, :string
   end
 end
