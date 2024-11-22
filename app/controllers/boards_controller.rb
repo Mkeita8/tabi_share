95a8a377d0cc5d2a9f06a3f370b88507ead45731
宮本敬太 <miyamotokeita@miyamotokeitanoMacBook-Air.local>
2024-11-22 18:08:08 +0900

    投稿機能

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,25 @@
 class BoardsController < ApplicationController
+  def new
+    @board = Board.new
+  end
+
   def index
     @boards = Board.includes(:user)
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: "投稿を作成しました"
+    else
+      flash.now[:danger] = "投稿を作成出来ませんでした"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_20_065353) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_20_090519) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
