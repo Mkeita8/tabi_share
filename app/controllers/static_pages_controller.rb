3cae7af98842f4bf90fd7d213e9ab6a05b95b36c
宮本敬太 <miyamotokeita@miyamotokeitanoMacBook-Air.local>
2025-02-25 16:05:38 +0900

    投稿詳細変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,7 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[board_top index_top]
-  def board_top
+  skip_before_action :require_login, only: %i[top]
+  def top
     @boards = Board.all
     @boards = Board.includes(:user).order(created_at: :desc).page(params[:page])
   end
-
-  def index_top
-    @q = Board.ransack(params[:q])
-    @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page]).per(9)
-  end
-
-  def about; end
 end
