6dbaa62cf6fd0112a9d996fc44621f20eb9eb1f1
宮本敬太 <miyamotokeita@miyamotokeitanoMacBook-Air.local>
2025-03-01 00:26:54 +0900

    利用規約、プライバシーポリシー表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top terms privacy]
   def top
     @boards = Board.all
     @boards = Board.includes(:user).order(created_at: :desc).page(params[:page])
   end
+
+  def terms; end
+
+  def privacy; end
 end
