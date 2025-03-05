667aa7614af5fa42ee28677bbccda02ad1d6f167
宮本敬太 <miyamotokeita@miyamotokeitanoMacBook-Air.local>
2025-03-05 20:11:42 +0900

    carrierwave.rb修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-require "carrierwave/storage/abstract"
+require "carrierwave/storage/abstract'"
 require "carrierwave/storage/file"
 require "carrierwave/storage/fog"
 
@@ -8,9 +8,10 @@ CarrierWave.configure do |config|
     config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.fog_public = false
     config.fog_credentials = {
-      provider: "AWS",
+      provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION'],
     }
   else
     config.storage :file
